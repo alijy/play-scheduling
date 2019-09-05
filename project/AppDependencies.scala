1eee0c195ec30c42434be4e1ff665f518f19f948
Alex Olkhovskiy <lxol@users.noreply.github.com>
2019-09-05 08:33:57 +0100

    wip

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,10 +11,12 @@ object AppDependencies {
 
   val test: Seq[ModuleID] =
     Seq(
-      "org.scalatest"          %% "scalatest"          % "3.0.5"          % "test",
+      //"org.scalatest"          %% "scalatest"          % "3.0.5"          % "test",
+      "org.scalatest"          %% "scalatest"          % "2.2.6"          % "test",
       "org.pegdown"            % "pegdown"             % "1.6.0"          % "test",
-      "com.typesafe.play" %% "play-netty-server"       % "2.5.19" % "test",
-      "ch.qos.logback"         % "logback-classic"     % "1.2.3"          % "test",
+      //"com.typesafe.play" %% "play-netty-server"       % "2.5.19" % "test",
+      //"io.netty" %% "netty"       % "3.8.0.Final" % "test",
+      //"ch.qos.logback"         % "logback-classic"     % "1.2.3"          % "test",
       "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1"          % "test",
       "uk.gov.hmrc"            %% "reactivemongo-test" % "4.15.0-play-25" % "test"
     )
