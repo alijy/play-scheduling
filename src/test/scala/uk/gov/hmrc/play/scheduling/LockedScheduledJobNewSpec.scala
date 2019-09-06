bf29d63f7da4df4ecb953c3eb02b03f6ffa5a714
Alex Olkhovskiy <lxol@users.noreply.github.com>
2019-09-06 10:36:51 +0100

    wip

---
+++ src/test/scala/uk/gov/hmrc/play/scheduling/LockedScheduledJobNewSpec.scala
@@ -0,0 +1,367 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.lock
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.{CountDownLatch, TimeUnit}
+
+import org.joda.time.{DateTime, Duration}
+import org.scalatest._
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+import org.scalatest.enablers.Emptiness
+import reactivemongo.api.commands.LastError
+import uk.gov.hmrc.lock.LockFormats.Lock
+import uk.gov.hmrc.mongo.{Awaiting, MongoSpecSupport, ReactiveRepository}
+import uk.gov.hmrc.play.scheduling.LockedScheduledJob
+import uk.gov.hmrc.time.DateTimeUtils
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.FiniteDuration
+
+
+class LockRepositorySpec extends WordSpecLike with Matchers with MongoSpecSupport with Awaiting with BeforeAndAfterEach with OptionValues with ScalaFutures with IntegrationPatience {
+  import scala.concurrent.Future
+
+  private implicit val now = DateTimeUtils.now
+  val lockId = "testLock"
+  val owner = "repoSpec"
+  val testContext = this
+
+  implicit def liftFuture[A](v: A) = Future.successful(v)
+
+  implicit val reactiveRepositoryEmptiness = new Emptiness[ReactiveRepository[_, _]] {
+    override def isEmpty(thing: ReactiveRepository[_, _]) = await(thing.count) == 0
+  }
+
+  val repo = new LockRepository {
+    val retryIntervalMillis = FiniteDuration(5L, TimeUnit.SECONDS).toMillis
+
+    override def withCurrentTime[A](f: (DateTime) => A) = f(now)
+  }
+
+  override protected def beforeEach(): Unit = {
+    await(repo.collection.db.connection.active)
+    await(repo.removeAll())
+  }
+
+  def manuallyInsertLock(lock: Lock) = {
+    await(repo.insert(lock))
+  }
+
+    class SimpleJob(val name: String) extends LockedScheduledJob {
+
+        override val releaseLockAfter = new Duration(300000)
+
+        val start = new CountDownLatch(1)
+        ///implicit val mongoi = mongo
+        //val lockRepository = new LockRepository()
+        // val lockRepository = new LockRepository()
+
+      val lockRepository = new LockRepository {
+        val retryIntervalMillis = FiniteDuration(5L, TimeUnit.SECONDS).toMillis
+
+        override def withCurrentTime[A](f: (DateTime) => A) = f(now)
+      }
+
+        def continueExecution(): Unit = start.countDown()
+        val executionCount = new AtomicInteger(0)
+        def executions: Int = executionCount.get()
+        override def executeInLock(implicit ec: ExecutionContext): Future[Result] =
+            Future {
+                start.await(1, TimeUnit.MINUTES)
+                Result(executionCount.incrementAndGet().toString)
+            }
+
+        override def initialDelay = FiniteDuration(1, TimeUnit.MINUTES)
+
+        override def interval = FiniteDuration(1, TimeUnit.MINUTES)
+
+    }
+
+    "LockedScheduledJob" should {
+
+        "let job run in sequence" in {
+            val job = new SimpleJob("job1")
+            job.continueExecution()
+            //await(repo.lock(lockId, owner, new Duration(1000L))) shouldBe true
+            await(job.execute).message shouldBe "Job with job1 run and completed with result 1"
+            await(job.execute).message shouldBe "Job with job1 run and completed with result 2"
+        }
+    }
+    "not allow job to run in parallel" in {
+        val job = new SimpleJob("job2")
+
+        val pausedExecution = job.execute
+        pausedExecution.isCompleted     shouldBe false
+        job.isRunning.futureValue       shouldBe true
+        job.execute.futureValue.message shouldBe "Job with job2 cannot aquire mongo lock, not running"
+        job.isRunning.futureValue       shouldBe true
+
+        job.continueExecution()
+        pausedExecution.futureValue.message shouldBe "Job with job2 run and completed with result 1"
+        job.isRunning.futureValue           shouldBe false
+    }
+
+
+
+    "The lock method" should {
+
+    "successfully create a lock if one does not already exist" in {
+      await(repo.lock(lockId, owner, new Duration(1000L))) shouldBe true
+
+      val lock = await(repo.findAll())
+
+      lock.head shouldBe Lock(lockId, owner, now, now.plusSeconds(1))
+    }
+
+    "successfully create a lock if a different one already exists" in {
+      manuallyInsertLock(Lock("nonMatchingLock", owner, now, now.plusSeconds(1)))
+
+      await(repo.lock(lockId, owner, new Duration(1000L))) shouldBe true
+      await(repo.count) shouldBe 2
+
+      await(repo.findById(lockId)) shouldBe Some(Lock(lockId, owner, now, now.plusSeconds(1)))
+    }
+
+    "do not change a non-expired lock with a different owner" in {
+      val alternativeOwner = "owner2"
+      manuallyInsertLock(Lock(lockId, alternativeOwner, now, now.plusSeconds(100)))
+
+      await(repo.lock(lockId, owner, new Duration(1000L))) shouldBe false
+      await(repo.findById(lockId)).map(_.owner) shouldBe Some(alternativeOwner)
+    }
+
+    "do not change a non-expired lock with the same owner" in {
+
+      await(repo.removeAll())
+
+      val existingLock = Lock(lockId, owner, now.minusDays(1), now.plusDays(1))
+
+      manuallyInsertLock(existingLock)
+
+      await(repo.lock(lockId, owner, new Duration(1000L))) shouldBe false
+
+      await(repo.findAll()).head shouldBe existingLock
+
+    }
+
+    "change an expired lock" in {
+      val expiredLock = Lock(lockId, owner, now.minusDays(2), now.minusDays(1))
+
+      manuallyInsertLock(expiredLock)
+
+      val gotLock = await(repo.lock(lockId, owner, new Duration(1000L)))
+
+      gotLock shouldBe true
+      await(repo.findAll()).head shouldBe Lock(lockId, owner, now, now.plusSeconds(1))
+    }
+  }
+
+  "The renew method" should {
+    "not renew a lock if one does not already exist" in {
+      await(repo.renew(lockId, owner, new Duration(1000L))) shouldBe false
+      await(repo.findAll()) shouldBe empty
+    }
+
+    "not renew a different lock if one exists" in {
+      manuallyInsertLock(Lock("nonMatchingLock", owner, now, now.plusSeconds(1)))
+
+      await(repo.renew(lockId, owner, new Duration(1000L))) shouldBe false
+      await(repo.findAll()).head shouldBe Lock("nonMatchingLock", owner, now, now.plusSeconds(1))
+    }
+
+    "not change a non-expired lock with a different owner" in {
+      val alternativeOwner = "owner2"
+      manuallyInsertLock(Lock(lockId, alternativeOwner, now, now.plusSeconds(100)))
+
+      await(repo.renew(lockId, owner, new Duration(1000L))) shouldBe false
+      await(repo.findById(lockId)).map(_.owner) shouldBe Some(alternativeOwner)
+    }
+
+    "change a non-expired lock with the same owner" in {
+      val existingLock = Lock(lockId, owner, now.minusDays(1), now.plusDays(1))
+
+      manuallyInsertLock(existingLock)
+
+      await(repo.renew(lockId, owner, new Duration(1000L))) shouldBe true
+
+      await(repo.findAll()).head shouldBe Lock(lockId, owner, existingLock.timeCreated, now.plus(new Duration(1000L)))
+    }
+
+    "not renew an expired lock" in {
+      val expiredLock = Lock(lockId, owner, now.minusDays(2), now.minusDays(1))
+
+      manuallyInsertLock(expiredLock)
+
+      val gotLock = await(repo.renew(lockId, owner, new Duration(1000L)))
+
+      gotLock shouldBe false
+      await(repo.findAll()).head shouldBe expiredLock
+    }
+  }
+
+  "The releaseLock method" should {
+
+    "remove an owned and expired lock" in {
+      val lock = Lock(lockId, owner, now.minusDays(2), now.minusDays(1))
+      manuallyInsertLock(lock)
+
+      await(repo.releaseLock(lockId, owner))
+
+      await(repo.count) shouldBe 0
+    }
+
+    "remove an owned and unexpired lock" in {
+      val lock = Lock(lockId, owner, now.minusDays(1), now.plusDays(1))
+      manuallyInsertLock(lock)
+
+      await(repo.releaseLock(lockId, owner))
+
+      await(repo.count) shouldBe 0
+    }
+
+    "do nothing if the lock doesn't exist" in {
+      await(repo.releaseLock(lockId, owner))
+
+      await(repo.count) shouldBe 0
+    }
+
+    "leave an expired lock owned by someone else" in {
+
+      val someoneElsesExpiredLock = Lock(lockId, "someoneElse", now.minusDays(2), now.minusDays(1))
+      manuallyInsertLock(someoneElsesExpiredLock)
+
+      await(repo.releaseLock(lockId, owner))
+
+      await(repo.findAll()).head shouldBe someoneElsesExpiredLock
+    }
+
+    "leave an unexpired lock owned by someone else" in {
+
+      val someoneElsesLock = Lock(lockId, "someoneElse", now.minusDays(2), now.plusDays(1))
+      manuallyInsertLock(someoneElsesLock)
+
+      await(repo.releaseLock(lockId, owner))
+
+      await(repo.findAll()).head shouldBe someoneElsesLock
+    }
+
+    "leave a different owned lock" in {
+
+      val someOtherLock = Lock("someOtherLock", owner, now.minusDays(1), now.plusDays(1))
+      manuallyInsertLock(someOtherLock)
+
+      await(repo.releaseLock(lockId, owner))
+
+      await(repo.findAll()).head shouldBe someOtherLock
+    }
+  }
+
+  "The isLocked method" should {
+    "return false if no lock obtained" in {
+      await(repo.isLocked(lockId, owner)) should be (false)
+    }
+
+    "return true if lock held" in {
+      manuallyInsertLock(Lock(lockId, owner, now, now.plusSeconds(100)))
+      await(repo.isLocked(lockId, owner)) should be (true)
+    }
+
+    "return false if the lock is held but expired" in {
+      manuallyInsertLock(Lock(lockId, owner, now.minusDays(2), now.minusDays(1)))
+      await(repo.isLocked(lockId, owner)) should be (false)
+    }
+  }
+
+  "The lock keeper" should {
+    val lockKeeper = new LockKeeper {
+      val forceLockReleaseAfter = Duration.standardSeconds(1)
+      override lazy val serverId: String = testContext.owner
+      val lockId: String = testContext.lockId
+      val repo: LockRepository = testContext.repo
+    }
+
+    "run the block supplied if the lock can be obtained, and return an option on the result and release the lock" in {
+      def hasLock = {
+        await(repo.findById(lockId)) shouldBe Some(Lock(lockId, owner, now, now.plusSeconds(1)))
+        Future.successful("testString")
+      }
+
+      await(lockKeeper.tryLock[String](hasLock)) shouldBe Some("testString")
+      repo shouldBe empty
+    }
+
+    "run the block supplied and release the lock even if the block returns a failed future" in {
+      a [RuntimeException] should be thrownBy{
+        await(lockKeeper.tryLock(Future.failed(new RuntimeException)))
+      }
+      repo shouldBe empty
+    }
+
+    "run the block supplied and release the lock even if the block throws an exception" in {
+      a [RuntimeException] should be thrownBy{
+        await(lockKeeper.tryLock(throw new RuntimeException ))
+      }
+      repo shouldBe empty
+    }
+
+    "not run the block supplied if the lock is owned by someone else, and return None" in {
+      val manualyInsertedLock = Lock(lockId, "owner2", now, now.plusSeconds(100))
+      manuallyInsertLock(manualyInsertedLock)
+
+      await(lockKeeper.tryLock {
+        fail("Should not be run!")
+      }) shouldBe None
+
+      await(repo.findAll()).head shouldBe manualyInsertedLock
+    }
+
+    "not run the block supplied if the lock is already owned by the caller, and return None" in {
+      val manualyInsertedLock = Lock(lockId, owner, now, now.plusSeconds(100))
+      manuallyInsertLock(manualyInsertedLock)
+
+      await(lockKeeper.tryLock {
+        fail("Should not be run!")
+      }) shouldBe None
+
+      await(repo.findAll()).head shouldBe manualyInsertedLock
+    }
+
+    "return false from isLocked if no lock obtained" in {
+      await(lockKeeper.isLocked) shouldBe false
+    }
+
+    "return true from isLocked if lock held" in {
+      manuallyInsertLock(Lock(lockId, owner, now, now.plusSeconds(100)))
+      await(lockKeeper.isLocked) shouldBe true
+    }
+  }
+
+
+  "Mongo should" should {
+    val DuplicateKey = 11000
+    "throw an exception if a lock object is inserted that is not unique" in {
+      val lock1 = Lock("lockName", "owner1", now.plusDays(1), now.plusDays(2))
+      val lock2 = Lock("lockName", "owner2", now.plusDays(3), now.plusDays(4))
+      manuallyInsertLock(lock1)
+
+      val error = the[LastError] thrownBy manuallyInsertLock(lock2)
+      error.code should contain(DuplicateKey)
+
+      await(repo.findAll()).head shouldBe lock1
+    }
+  }
+}
--- src/test/scala/uk/gov/hmrc/play/scheduling/LockedScheduledJobSpec.scala
+++ src/test/scala/uk/gov/hmrc/play/scheduling/LockedScheduledJobSpec.scala
@@ -14,114 +14,144 @@
  * limitations under the License.
  */
 
-package uk.gov.hmrc.play.scheduling
+// /*
+//  * Copyright 2019 HM Revenue & Customs
+//  *
+//  * Licensed under the Apache License, Version 2.0 (the "License");
+//  * you may not use this file except in compliance with the License.
+//  * You may obtain a copy of the License at
+//  *
+//  *     http://www.apache.org/licenses/LICENSE-2.0
+//  *
+//  * Unless required by applicable law or agreed to in writing, software
+//  * distributed under the License is distributed on an "AS IS" BASIS,
+//  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  * See the License for the specific language governing permissions and
+//  * limitations under the License.
+//  */
 
-import java.util.concurrent.{CountDownLatch, TimeUnit}
-import java.util.concurrent.atomic.AtomicInteger
+// package uk.gov.hmrc.play.scheduling
 
-import org.joda.time.{DateTime, DateTimeZone, Duration}
-import org.scalatest.{BeforeAndAfterEach, Matchers, WordSpec}
-import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.time.{Millis, Seconds, Span}
-import org.scalatestplus.play.guice.GuiceOneAppPerTest
-import play.api.Application
-import play.api.inject.guice.GuiceApplicationBuilder
-import uk.gov.hmrc.lock.{LockMongoRepository, LockRepository}
-import uk.gov.hmrc.play.common.MongoSpecSupportFix
+// import java.util.concurrent.{CountDownLatch, TimeUnit}
+// import java.util.concurrent.atomic.AtomicInteger
 
-import scala.concurrent.{Await, ExecutionContext, Future}
-import scala.concurrent.ExecutionContext.Implicits._
-import scala.concurrent.duration._
-import scala.util.Try
+// import org.joda.time.{DateTime, DateTimeZone, Duration}
+// import org.scalatest.{BeforeAndAfterEach, Matchers, WordSpec}
+// import org.scalatest.concurrent.ScalaFutures
+// import org.scalatest.time.{Millis, Seconds, Span}
+// import org.scalatestplus.play.guice.GuiceOneAppPerTest
+// import play.api.Application
+// import play.api.inject.guice.GuiceApplicationBuilder
+// import uk.gov.hmrc.lock.{LockMongoRepository, LockRepository}
+// import uk.gov.hmrc.play.common.MongoSpecSupportFix
 
-class LockedScheduledJobSpec
-    extends WordSpec
-        with Matchers
-        with ScalaFutures
-        with GuiceOneAppPerTest
-        with MongoSpecSupportFix
-        with BeforeAndAfterEach {
+// import scala.concurrent.{Await, ExecutionContext, Future}
+// import scala.concurrent.ExecutionContext.Implicits._
+// import scala.concurrent.duration._
+// import scala.util.Try
+// import uk.gov.hmrc.time.DateTimeUtils
+// import uk.gov.hmrc.mongo.Awaiting
+// class LockedScheduledJobSpec
+//     extends WordSpec
+//         with Matchers
+//         with ScalaFutures
+//         with Awaiting
+//         //with GuiceOneAppPerTest
+//         with MongoSpecSupportFix
+//         with BeforeAndAfterEach {
+//   private implicit val now = DateTimeUtils.now
 
-    //  override def fakeApplication(): Application =
-    //    new GuiceApplicationBuilder()
-    //      .configure("mongodb.uri" -> "mongodb://localhost:27017/test-play-schedule")
-    //      .build()
+//     //  override def fakeApplication(): Application =
+//     //    new GuiceApplicationBuilder()
+//     //      .configure("mongodb.uri" -> "mongodb://localhost:27017/test-play-schedule")
+//     //      .build()
 
-    //override protected val  mongoUri = s"mongodb://127.0.0.1:27017/$databaseName?rm.monitorRefreshMS=1000&rm.failover=default"
-        //"mongodb://localhost:27017/test-play-schedule"
+//     //override protected val  mongoUri = s"mongodb://127.0.0.1:27017/$databaseName?rm.monitorRefreshMS=1000&rm.failover=default"
+//         //"mongodb://localhost:27017/test-play-schedule"
 
-    override implicit def patienceConfig: PatienceConfig = PatienceConfig(timeout = Span(1500, Millis), interval = Span(500, Millis))
+//     override implicit def patienceConfig: PatienceConfig = PatienceConfig(timeout = Span(1500, Millis), interval = Span(500, Millis))
 
-    class SimpleJob(val name: String) extends LockedScheduledJob {
+//     class SimpleJob(val name: String) extends LockedScheduledJob {
 
-        override val releaseLockAfter = new Duration(300000)
+//         override val releaseLockAfter = new Duration(300000)
 
-        val start = new CountDownLatch(1)
-        ///implicit val mongoi = mongo
-        val lockRepository = new LockRepository()
+//         val start = new CountDownLatch(1)
+//         ///implicit val mongoi = mongo
+//         //val lockRepository = new LockRepository()
+//         // val lockRepository = new LockRepository()
 
-        def continueExecution(): Unit = start.countDown()
+//       val lockRepository = new LockRepository {
+//         val retryIntervalMillis = FiniteDuration(5L, TimeUnit.SECONDS).toMillis
 
-        val executionCount = new AtomicInteger(0)
+//         override def withCurrentTime[A](f: (DateTime) => A) = f(now)
+//       }
+//         override protected def beforeEach(): Unit = {
+//             await(lockRepository.collection.db.connection.active)
+//             await(lockRepository.removeAll())
+//         }
 
-        def executions: Int = executionCount.get()
+//         def continueExecution(): Unit = start.countDown()
 
-        override def executeInLock(implicit ec: ExecutionContext): Future[Result] =
-            Future {
-                start.await(1, TimeUnit.MINUTES)
-                Result(executionCount.incrementAndGet().toString)
-            }
+//         val executionCount = new AtomicInteger(0)
 
-        override def initialDelay = FiniteDuration(1, TimeUnit.MINUTES)
+//         def executions: Int = executionCount.get()
 
-        override def interval = FiniteDuration(1, TimeUnit.MINUTES)
+//         override def executeInLock(implicit ec: ExecutionContext): Future[Result] =
+//             Future {
+//                 start.await(1, TimeUnit.MINUTES)
+//                 Result(executionCount.incrementAndGet().toString)
+//             }
 
-    }
+//         override def initialDelay = FiniteDuration(1, TimeUnit.MINUTES)
 
-    "LockedScheduledJob" should {
+//         override def interval = FiniteDuration(1, TimeUnit.MINUTES)
 
-        // "let job run in sequence" in {
-        //   val job = new SimpleJob("job1")
-        //   job.continueExecution()
-        //   Await.result(job.execute, 1.minute).message shouldBe "Job with job1 run and completed with result 1"
-        //   Await.result(job.execute, 1.minute).message shouldBe "Job with job1 run and completed with result 2"
-        // }
+//     }
 
-        "not allow job to run in parallel" in {
-            val job = new SimpleJob("job2")
+//     "LockedScheduledJob" should {
 
-            val pausedExecution = job.execute
-            pausedExecution.isCompleted shouldBe false
-            Thread.sleep(500)
-            //Await.result(job.isRunning, 1.minute) shouldBe  true
-            //job.isRunning.futureValue       shouldBe true
+//         // "let job run in sequence" in {
+//         //   val job = new SimpleJob("job1")
+//         //   job.continueExecution()
+//         //   Await.result(job.execute, 1.minute).message shouldBe "Job with job1 run and completed with result 1"
+//         //   Await.result(job.execute, 1.minute).message shouldBe "Job with job1 run and completed with result 2"
+//         // }
 
-            val job2 = new SimpleJob("job2")
-            Await.result(job2.execute, 5.minute).message shouldBe "Job with job2 cannot aquire mongo lock, not running"
-            //job.execute.futureValue.message shouldBe "Job with job2 cannot aquire mongo lock, not running"
-            //job.isRunning.futureValue       shouldBe true
+//         "not allow job to run in parallel" in {
+//             val job = new SimpleJob("job2")
 
-            job2.continueExecution()
-            Thread.sleep(500)
-            Await.result(pausedExecution, 5.minute).message shouldBe "Job with job2 run and completed with result 1"
-            // pausedExecution.futureValue.message shouldBe "Job with job2 run and completed with result 1"
-            Await.result(job2.isRunning, 5.minute) shouldBe false
-            //job.isRunning.futureValue           shouldBe false
-        }
+//             val pausedExecution = job.execute
+//             pausedExecution.isCompleted shouldBe false
+//             Thread.sleep(500)
+//             //Await.result(job.isRunning, 1.minute) shouldBe  true
+//             //job.isRunning.futureValue       shouldBe true
 
-        // "should tolerate exceptions in execution" in {
-        //   val job = new SimpleJob("job3") {
-        //     override def executeInLock(implicit ec: ExecutionContext): Future[Result] = throw new RuntimeException
-        //   }
+//             val job2 = new SimpleJob("job2")
+//             Await.result(job2.execute, 5.minute).message shouldBe "Job with job2 cannot aquire mongo lock, not running"
+//             //job.execute.futureValue.message shouldBe "Job with job2 cannot aquire mongo lock, not running"
+//             //job.isRunning.futureValue       shouldBe true
 
-        //   Try(
-        //  Await.result(job.execute, 1.minute)
-        //     //job.execute.futureValue
-        //   )
+//             job2.continueExecution()
+//             Thread.sleep(500)
+//             Await.result(pausedExecution, 5.minute).message shouldBe "Job with job2 run and completed with result 1"
+//             // pausedExecution.futureValue.message shouldBe "Job with job2 run and completed with result 1"
+//             Await.result(job2.isRunning, 5.minute) shouldBe false
+//             //job.isRunning.futureValue           shouldBe false
+//         }
 
-        //  Await.result(job.isRunning, 1.minute) shouldBe false
-        //   // job.isRunning.futureValue shouldBe false
-        // }
-    }
+//         // "should tolerate exceptions in execution" in {
+//         //   val job = new SimpleJob("job3") {
+//         //     override def executeInLock(implicit ec: ExecutionContext): Future[Result] = throw new RuntimeException
+//         //   }
 
-}
+//         //   Try(
+//         //  Await.result(job.execute, 1.minute)
+//         //     //job.execute.futureValue
+//         //   )
+
+//         //  Await.result(job.isRunning, 1.minute) shouldBe false
+//         //   // job.isRunning.futureValue shouldBe false
+//         // }
+//     }
+
+// }
